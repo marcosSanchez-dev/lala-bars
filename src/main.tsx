327aa81b28f2677ebe05ceba9aa3b99cd1f81fa4
Marcos <marcos.sanchez@saei.mx>
2025-07-07 20:06:34 -0600

    try to fix deploy bug 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/bebas-neue"; // Regular 400 por defecto
 import "./index.css";
 import App from "./App.tsx";
 
