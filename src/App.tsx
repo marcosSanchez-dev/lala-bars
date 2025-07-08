1cfee4d13f4a2f71eba1be69231faf142faec46f
Marcos <marcos.sanchez@saei.mx>
2025-07-07 19:22:53 -0600

    Fix alignment and layout scalability issues in the view

--- src/App.tsx
+++ src/App.tsx
@@ -21,35 +21,33 @@ function App() {
       <img
         src={LalaLogo}
         alt="Lala Logo"
-        className="absolute top-8 right-12 w-[150px] object-contain"
+        className="absolute top-4 right-6 w-[120px] object-contain"
       />
 
-      <div className="w-[1440px] h-[900px] grid grid-cols-3 items-center px-12">
+      <div className="w-full max-w-screen-xl h-[90%] grid grid-cols-3 items-center px-8">
         {/* Columna Izquierda */}
-        <div className="flex flex-col justify-between h-full py-8">
-          <div className="text-left space-y-2">
-            <div className="flex items-center gap-2 text-sm">
-              <img src={ClockIcon} alt="Clock" className="w-12 h-12" />
-              <div className="text-[40px]">TIEMPO</div>
+        <div className="flex flex-col justify-between h-full py-8 text-right pr-4">
+          <div className="space-y-2">
+            <div className="flex flex-col items-end gap-1">
+              <img src={ClockIcon} alt="Clock" className="w-10 h-10" />
+              <div className="text-[32px]">TIEMPO</div>
             </div>
-            <div className="text-[160px] leading-[140px]">00:00</div>
-            <div className="text-[80px] text-center">:00</div>
+            <div className="text-[140px] leading-[120px]">00:00</div>
+            <div className="text-[64px]">:00</div>
           </div>
-          <div className="flex flex-col items-start gap-1">
-            <div className="flex items-center gap-2 text-[50px]">
-              <span>JUGADOR</span>
-            </div>
-            <div className="text-[100px] flex items-center">
-              <img src={RunIcon} alt="Jugador 1" className="w-20 h-20" />1
+          <div className="flex flex-col items-end gap-1">
+            <div className="text-[40px]">JUGADOR</div>
+            <div className="text-[80px] flex items-center gap-3">
+              <img src={RunIcon} alt="Jugador 1" className="w-14 h-14" />1
             </div>
           </div>
         </div>
 
         {/* Columnas del medio (barras) */}
-        <div className="flex justify-center gap-20 items-end h-full">
+        <div className="flex justify-center gap-28 items-end h-full">
           {/* Barra Jugador 1 */}
           <div
-            className="relative w-[90px] h-full bg-[#f6f5f2] rounded-[20px] shadow-[inset_0_10px_20px_rgba(0,0,0,0.1)] flex items-end cursor-pointer"
+            className="relative w-[80px] h-full bg-[#f6f5f2] rounded-[20px] shadow-[inset_0_10px_20px_rgba(0,0,0,0.1)] flex items-end cursor-pointer"
             onClick={() => increase(setProgress1)}
           >
             <div
@@ -65,7 +63,7 @@ function App() {
 
           {/* Barra Jugador 2 */}
           <div
-            className="relative w-[90px] h-full bg-[#f6f5f2] rounded-[20px] shadow-[inset_0_10px_20px_rgba(0,0,0,0.1)] flex items-end cursor-pointer"
+            className="relative w-[80px] h-full bg-[#f6f5f2] rounded-[20px] shadow-[inset_0_10px_20px_rgba(0,0,0,0.1)] flex items-end cursor-pointer"
             onClick={() => increase(setProgress2)}
           >
             <div
@@ -81,22 +79,20 @@ function App() {
         </div>
 
         {/* Columna Derecha */}
-        <div className="flex flex-col justify-between h-full py-8 items-end">
-          <div className="text-right space-y-2">
-            <div className="flex items-center justify-end gap-2 text-sm">
-              <img src={ClockIcon} alt="Clock" className="w-12 h-12" />
-              <div className="text-[40px]">TIEMPO</div>
+        <div className="flex flex-col justify-between h-full py-8 text-left pl-4">
+          <div className="space-y-2">
+            <div className="flex flex-col items-start gap-1">
+              <img src={ClockIcon} alt="Clock" className="w-10 h-10" />
+              <div className="text-[32px]">TIEMPO</div>
             </div>
-            <div className="text-[160px] leading-[140px]">00:00</div>
-            <div className="text-[80px] text-center">:00</div>
+            <div className="text-[140px] leading-[120px]">00:00</div>
+            <div className="text-[64px]">:00</div>
           </div>
-          <div className="flex flex-col items-end gap-1">
-            <div className="flex items-center gap-2 text-[50px]">
-              <span>JUGADOR</span>
-            </div>
-            <div className="text-[100px] flex items-center">
+          <div className="flex flex-col items-start gap-1">
+            <div className="text-[40px]">JUGADOR</div>
+            <div className="text-[80px] flex items-center gap-3">
               2
-              <img src={RunIcon} alt="Jugador 2" className="w-20 h-20" />
+              <img src={RunIcon} alt="Jugador 2" className="w-14 h-14" />
             </div>
           </div>
         </div>
